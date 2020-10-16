f70599c2465bef87acfdcb18449a86c246226c51
Sara Cemal <saracemal@gmail.com>
2020-10-16 04:23:59 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -51,23 +51,14 @@ def add_characters
          ]
       }
    }
-
-   # Write code below that does the following programmatically:
-   #
+\
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
    # - Add two hashes to this array, both with a :name key.
-   #   - The first hash should have the :name key set to "Prince Escalus" 
-   #   - The second hash should have the :name key set to "Apothecary" 
-   # Code your solution here:
+   
    
    :additional_characters => {name: "Prince Escalus"}
    
    :additional_characters => {name: "Apothecary"}
-
-
-
-
-
 
    # After your code, the following line will return the altered hash
    epic_tragedy
